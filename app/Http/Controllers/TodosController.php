9ac2424ca49c13573014508ac98a373efb30e108
reallithuanianmafia <reallithuanianmafia@outlook.com>
2021-02-24 00:42:35 +0400

    no message

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TodosController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return 'salam';
+    }
+    //
+}
